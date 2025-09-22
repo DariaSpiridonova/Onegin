d50cdfa89305bc6ca4a5046785def8f9a5ce4340
DariaSpiridonova <spiridonova.da@phystech.edu>
2025-09-22 16:56:11 +0300

    add structures

--- onegin.cpp
+++ onegin.cpp
@@ -8,78 +8,62 @@
 int open_files_success(FILE *read_strings, FILE *sorted_strings);
 size_t return_num_of_bytes_in_file(FILE *fp);
 size_t return_num_of_strings_in_file(char *buffer);
+struct data_of_buffer read_to_buffer_from_file(FILE *read_strings);
 void add_pointers_on_strings(char *buffer, char **pointers_on_strings, size_t num_of_strings_in_file);
+struct strings_info get_num_of_strings_and_array_of_points(char *buffer);
 void sort_array_of_points(char **pointers_on_strings, size_t num_of_strings_in_file, int (*my_strcmp)(const char *str1, const char *str2));
-/*int compare_str_from_the_begining(const void *par1, const void *par2);
-int compare_str_from_the_end(const void *par1, const void *par2);*/
+int compare_str_from_the_begining(const void *par1, const void *par2);
+int compare_str_from_the_end(const void *par1, const void *par2);
 
 void write_data_for_a_file(char **pointers_on_strings, size_t num_of_strings_in_file, FILE *file_for_recording);
+
+struct data_of_buffer
+{
+    size_t num_of_bytes_in_file;
+    char *buffer;
+};
+
+struct strings_info
+{
+    size_t num_of_strings_in_file;
+    char **pointers_on_strings;
+};
 
 int main()
 {
     FILE *read_strings = fopen("original_strings.txt", "rb");
     FILE *sorted_strings = fopen("sorted_strings.txt", "wb");
 
-    if(!open_files_success(read_strings, sorted_strings)) return -1; 
+    if (!open_files_success(read_strings, sorted_strings))
+        return -1;
 
-    size_t num_of_bytes_in_file = return_num_of_bytes_in_file(read_strings);
-
-    printf("%d\n", num_of_bytes_in_file); // проверка количества символов в файле
-
-    char *buffer = (char *)calloc(num_of_bytes_in_file + 1, sizeof(char));
-    if (buffer == NULL)
+    struct data_of_buffer data_in_buffer = read_to_buffer_from_file(read_strings);
+    if (data_in_buffer.buffer == 0 || data_in_buffer.num_of_bytes_in_file == 0)
         return 2;
 
-    size_t num_success_read_symbols = fread(buffer, sizeof(char), num_of_bytes_in_file, read_strings);
-    if (num_success_read_symbols < num_of_bytes_in_file)
+    struct strings_info strings = get_num_of_strings_and_array_of_points(data_in_buffer.buffer);
+    if (strings.num_of_strings_in_file == 0 || strings.pointers_on_strings == NULL)
         return 3;
 
-    printf("%d\n", num_success_read_symbols); // проверка количества успешно прочитанных в буфер символов
+    sort_array_of_points(strings.pointers_on_strings, strings.num_of_strings_in_file, my_strcmp_from_the_begining);
+    write_data_for_a_file(strings.pointers_on_strings, strings.num_of_strings_in_file, sorted_strings);
 
-    buffer[num_success_read_symbols + 1] = '\0';
-    printf("%s\n", buffer); // проверка содержимого буфера
+    sort_array_of_points(strings.pointers_on_strings, strings.num_of_strings_in_file, my_strcmp_from_the_end);
+    write_data_for_a_file(strings.pointers_on_strings, strings.num_of_strings_in_file, sorted_strings);
 
-    size_t num_of_strings_in_file = return_num_of_strings_in_file(buffer);
-    printf("%d\n", num_of_strings_in_file); // проверка количества строк в файле
+    qsort(strings.pointers_on_strings, strings.num_of_strings_in_file, sizeof(char *), compare_str_from_the_begining);
+    write_data_for_a_file(strings.pointers_on_strings, strings.num_of_strings_in_file, sorted_strings);
 
-    char **pointers_on_strings = (char **)calloc(num_of_strings_in_file, sizeof(char *));
-    add_pointers_on_strings(buffer, pointers_on_strings, num_of_strings_in_file);
+    qsort(strings.pointers_on_strings, strings.num_of_strings_in_file, sizeof(char *), compare_str_from_the_end);
+    write_data_for_a_file(strings.pointers_on_strings, strings.num_of_strings_in_file, sorted_strings);
 
-    for (size_t i = 0; i < num_of_strings_in_file; i++) // проверка содержимого массива указателей
-    {
-        printf("%d: ", i);
-        printf("<%s>", pointers_on_strings[i]);
-    }
+    sort_array_of_points(strings.pointers_on_strings, strings.num_of_strings_in_file, my_strcmp_in_order);
+    write_data_for_a_file(strings.pointers_on_strings, strings.num_of_strings_in_file, sorted_strings);
 
-    char **sources_pointers_on_strings = pointers_on_strings;
-
-    sort_array_of_points(pointers_on_strings, num_of_strings_in_file, my_strcmp_from_the_begining);
-    for (size_t i = 0; i < num_of_strings_in_file; i++) // проверка содержимого массива указателей
-    {
-        printf("%d: ", i);
-        printf("<%s>", pointers_on_strings[i]);
-    }
-
-    write_data_for_a_file(pointers_on_strings, num_of_strings_in_file, sorted_strings);
-    
-    sort_array_of_points(pointers_on_strings, num_of_strings_in_file, my_strcmp_from_the_end);
-    for (size_t i = 0; i < num_of_strings_in_file; i++) // проверка содержимого массива указателей
-    {
-        printf("%d: ", i);
-        printf("<%s>", pointers_on_strings[i]);
-    }
-    write_data_for_a_file(pointers_on_strings, num_of_strings_in_file, sorted_strings);
-
-    /*qsort(pointers_on_strings, num_of_strings_in_file, sizeof(char *), compare_str_from_the_begining);
-    write_data_for_a_file(pointers_on_strings, num_of_strings_in_file, sorted_strings);
-
-    qsort(pointers_on_strings, num_of_strings_in_file, sizeof(char *), compare_str_from_the_end);
-    write_data_for_a_file(pointers_on_strings, num_of_strings_in_file, sorted_strings);*/
-
-    write_data_for_a_file(sources_pointers_on_strings, num_of_strings_in_file, sorted_strings);
-
-    if (fclose(read_strings) == EOF) return -1;
-    if (fclose(sorted_strings) == EOF) return -1;
+    if (fclose(read_strings) == EOF)
+        return -1;
+    if (fclose(sorted_strings) == EOF)
+        return -1;
 
     return 0;
 }
@@ -109,6 +93,35 @@ size_t return_num_of_bytes_in_file(FILE *fp)
     return i;
 }
 
+struct data_of_buffer read_to_buffer_from_file(FILE *read_strings)
+{
+    assert(read_strings != NULL);
+
+    struct data_of_buffer data = {0, 0};
+
+    size_t num_of_bytes_in_file = return_num_of_bytes_in_file(read_strings);
+
+    printf("%d\n", num_of_bytes_in_file); // проверка количества символов в файле
+
+    char *buffer = (char *)calloc(num_of_bytes_in_file + 1, sizeof(char));
+    if (buffer == NULL)
+        return {0, 0};
+
+    size_t num_success_read_symbols = fread(buffer, sizeof(char), num_of_bytes_in_file, read_strings);
+    if (num_success_read_symbols < num_of_bytes_in_file)
+        return {0, 0};
+
+    printf("%d\n", num_success_read_symbols); // проверка количества успешно прочитанных в буфер символов
+
+    buffer[num_success_read_symbols] = '\0';
+    printf("%s\n", buffer); // проверка содержимого буфера
+
+    data.num_of_bytes_in_file = num_of_bytes_in_file;
+    data.buffer = buffer;
+
+    return data;
+}
+
 size_t return_num_of_strings_in_file(char *buffer)
 {
     assert(buffer != NULL);
@@ -136,12 +149,30 @@ void add_pointers_on_strings(char *buffer, char **pointers_on_strings, size_t nu
     for (; i < (num_of_strings_in_file - 1) && pointer_on_new_string != NULL; i++)
     {
         *pointer_on_new_string = '\0';
-        if (*(pointer_on_new_string - 1) == '\r') *(pointer_on_new_string - 1) = '\0';
+        if (*(pointer_on_new_string - 1) == '\r')
+            *(pointer_on_new_string - 1) = '\0';
         pointers_on_strings[i] = ptr_on_buffer;
         ptr_on_buffer = pointer_on_new_string + 1;
         pointer_on_new_string = strchr(ptr_on_buffer, '\n');
     }
     pointers_on_strings[i] = ptr_on_buffer;
+}
+
+struct strings_info get_num_of_strings_and_array_of_points(char *buffer)
+{
+    assert(buffer != NULL);
+
+    struct strings_info strings = {0, NULL};
+    size_t num_of_strings_in_file = return_num_of_strings_in_file(buffer);
+    printf("%d\n", num_of_strings_in_file); // проверка количества строк в файле
+
+    char **pointers_on_strings = (char **)calloc(num_of_strings_in_file, sizeof(char *));
+    add_pointers_on_strings(buffer, pointers_on_strings, num_of_strings_in_file);
+
+    strings.num_of_strings_in_file = num_of_strings_in_file;
+    strings.pointers_on_strings = pointers_on_strings;
+
+    return strings;
 }
 
 void sort_array_of_points(char **pointers_on_strings, size_t num_of_strings_in_file, int (*my_strcmp)(const char *str1, const char *str2))
@@ -151,7 +182,7 @@ void sort_array_of_points(char **pointers_on_strings, size_t num_of_strings_in_f
         for (size_t j = i + 1; j < num_of_strings_in_file; j++)
         {
             printf("%d\n", (*my_strcmp)((const char *)(pointers_on_strings[i]), (const char *)(pointers_on_strings[j])));
-            if ((*my_strcmp)((const char *)(pointers_on_strings[i]), (const char *)(pointers_on_strings[j])) < 0)
+            if ((*my_strcmp)((const char *)(pointers_on_strings[i]), (const char *)(pointers_on_strings[j])) > 0)
             {
                 char *ptr_help = pointers_on_strings[i];
                 pointers_on_strings[i] = pointers_on_strings[j];
@@ -164,27 +195,29 @@ void sort_array_of_points(char **pointers_on_strings, size_t num_of_strings_in_f
     }
 }
 
-/*
+/******************************************************/
 int compare_str_from_the_begining(const void *par1, const void *par2)
 {
-    const char *s1 = *(const char **) par1;
-    const char *s2 = *(const char **) par2;
+    const char *s1 = *(const char *const *)par1;
+    const char *s2 = *(const char *const *)par2;
 
     return my_strcmp_from_the_begining(s1, s2);
 }
 
-int compare_str_from_the_end(const void **par1, const void **par2)
+int compare_str_from_the_end(const void *par1, const void *par2)
 {
-    const char *s1 = *(const char **) par1;
-    const char *s2 = *(const char **) par2;
+    const char *s1 = *(const char *const *)par1;
+    const char *s2 = *(const char *const *)par2;
 
     return my_strcmp_from_the_end(s1, s2);
 }
-*/
+/******************************************************/
+
 void write_data_for_a_file(char **pointers_on_strings, size_t num_of_strings_in_file, FILE *file_for_recording)
 {
     for (size_t i = 0; i < num_of_strings_in_file; i++)
     {
         fprintf(file_for_recording, "%s\n", pointers_on_strings[i]);
     }
+    fprintf(file_for_recording, "\n");
 }
